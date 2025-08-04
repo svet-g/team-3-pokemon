749f340192ae70dabe5639fc2953e537774f8716
ha282 <habdi27a@gmail.com>
2025-08-04 16:19:21 +0100

    Adding markdown

--- main.py
+++ main.py
@@ -3,35 +3,66 @@ import pandas as pd
 import display_image as di
 import graph
 
-#Set screen with
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.markdown("""<HR>""", unsafe_allow_html=True,)
 
-#Set title of app
+# Set title of app
 st.title("Pick-Your-Pokemon")
 
-#Display a simple message
-st.write("Welcome to the Pick-Your-Pokemon app. Select your favourite pokemon to discover more about them :)")
+# Display a simple message
+st.markdown(
+    '''<h2 style="color: darkslategray;">Welcome to the Pick-Your-Pokemon app.</h2>
+    <h4 style="color: darkslategray;">
+    Select your favourite pokemon to discover more 
+    about them :)</a></h4>
+    ''',
+    unsafe_allow_html=True
+    )
 
 # Import some data into a Panda Dataframe
 df = pd.read_csv("pokemon.csv")
 
 pokemon = df["name"].to_list()
 
-option = st.selectbox(
-    "What pokemon do you want?",
-    pokemon
-)
+st.markdown("""<HR>""", unsafe_allow_html=True,)
 
-st.write(f"You selected: {option}")
+col1, col2 = st.columns(2, gap="large", vertical_alignment="top")
 
-pokemon_row = df.loc[df["name"] == option]
+st.markdown("""<HR>""", unsafe_allow_html=True,)
 
-st.metric(label="Weight (kg)", value=pokemon_row["weight_kg"].values[0])
-st.metric(label="Height (m)", value=pokemon_row["height_m"].values[0])
-st.metric(label="Main Ability", value=pokemon_row["ability_1"].values[0])
+with col1:    
+    st.markdown(
+        '''
+        <p></p>
+        <h5>What pokemon do you want?</h5>''',
+        unsafe_allow_html=True
+        )
+    
+    option = st.selectbox(
+        ' ',
+        pokemon
+        )
 
-# call display picture function
-di.view_image(df, option, pokemon)
+    pokemon_row = df.loc[df["name"] == option] 
+      
+    st.markdown('<BR>', unsafe_allow_html=True)
+    # st.markdown("""<HR>""", unsafe_allow_html=True,)
+    st.metric(label="Weight (kg)", value=pokemon_row["weight_kg"].values[0])
+    st.metric(label="Height (m)", value=pokemon_row["height_m"].values[0])
+    st.metric(label="Main Ability", value=pokemon_row["ability_1"].values[0])
+
+with col2:
+    # st.markdown('''<h3 style="color: darkslategray;">You selected:</h3>''',
+    #             unsafe_allow_html=True
+    #             )
+    # st.markdown(f'''<h4 style="color: darkslategray;">{option}</h4>''',
+    #             unsafe_allow_html=True
+    #             )
+    # st.markdown("""<HR>""", unsafe_allow_html=True,)
+    st.markdown('<BR>', unsafe_allow_html=True)
+    # call display picture function
+    di.view_image(df, option, pokemon)
 
 # display graph of weight vs height
 graph.show_graph(df, option)
+
+st.markdown("""<HR>""", unsafe_allow_html=True,)
