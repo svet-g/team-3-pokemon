d7cdefabfbefd5037691b258ab0c4975d244d5d2
Tomasz - DE <infotip@infotip.co.uk>
2025-08-04 13:25:54 +0100

    Tomasz: added main.py which is input_attributes.py + imported display_image module

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+import pandas as pd
+import display_image as di
+
+#Set title of app
+st.title("Pick-Your-Pokemon")
+
+#Display a simple message
+st.write("Welcome to the Pick-Your-Pokemon app. Select your favourite pokemon to discover more about them :)")
+
+# Import some data into a Panda Dataframe
+df = pd.read_csv("pokemon.csv")
+
+pokemon = df["name"].to_list()
+
+option = st.selectbox(
+    "What pokemon do you want?",
+    pokemon
+)
+
+st.write(f"You selected: {option}")
+
+pokemon_row = df.loc[df["name"] == option]
+
+st.write(pokemon_row[["name", "weight_kg", "height_m", "ability_1"]])
+
+#st.metric(pokemon_row[["name", "weight_kg", "height_m", "ability_1"]])
+
+st.metric(label="Weight (kg)", value=pokemon_row["weight_kg"].values[0])
+st.metric(label="Height (m)", value=pokemon_row["height_m"].values[0])
+st.metric(label="Main Ability", value=pokemon_row["ability_1"].values[0])
+
+# call display picture function
+di.view_image(df, option, pokemon)
